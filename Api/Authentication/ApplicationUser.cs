8b48da468079939bd35392a003a0fbf0f5ee6f23
Gilbert Ssenyonjo <gssenyonjo@gmail.com>
2024-12-21 18:25:37 +0300

    moved Api into its own repo

--- Api/Authentication/ApplicationUser.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace Api.Authentication;
-
-public class ApplicationUser : IdentityUser
-{
-
-}
--- Api/Authentication/Response.cs
+++
@@ -1,7 +0,0 @@
-namespace Api.Authentication;
-
-public class Response 
-{
-    public string Status { get;set;}
-    public string Message { get;set;}
-}
